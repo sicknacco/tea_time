3a5d16646230adc212cb4827b16ca0bbedb53abf
Nick Sacco <nicksacco10@gmail.com>
2023-10-31 13:03:46 -0700

    test: sad path testing for subscription creation. pausing to move to model validations

--- spec/requests/subscription_request_spec.rb
+++ spec/requests/subscription_request_spec.rb
@@ -50,4 +50,78 @@ RSpec.describe "POST Subscription", type: :request do
       expect(sub[:data][:attributes][:tea_id]).to eq(tea.id)
     end
   end
+
+  describe 'Sad Path' do
+    it 'returns an error if customer_id is missing' do
+      tea = create(:tea)
+
+      payload = {
+        title: 'The Good One',
+        price: 10.99,
+        status: 'active',
+        frequency: 'bi-monthly',
+        customer_id: '',
+        tea_id: tea.id
+      }
+      headers = {"CONTENT_TYPE" => "application/json"}
+
+      post '/api/v0/subscriptions', headers: headers, params: JSON.generate(payload)
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(400)
+
+      error = JSON.parse(response.body, symbolize_names: true)
+   
+      expect(error).to be_a(Hash)
+      expect(error).to have_key(:error)
+      expect(error[:error]).to eq("Customer must exist")
+    end
+
+    it 'returns an error if tea_id is missing' do
+      customer = create(:customer)
+
+      payload = {
+        title: 'The Good One',
+        price: 10.99,
+        status: 'active',
+        frequency: 'bi-monthly',
+        customer_id: customer.id,
+        tea_id: ''
+      }
+      headers = {"CONTENT_TYPE" => "application/json"}
+
+      post '/api/v0/subscriptions', headers: headers, params: JSON.generate(payload)
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(400)
+
+      error = JSON.parse(response.body, symbolize_names: true)
+
+      expect(error).to be_a(Hash)
+      expect(error).to have_key(:error)
+      expect(error[:error]).to eq("Tea must exist")
+    end
+
+    it 'returns an error if another attribute is missing' do
+      customer = create(:customer)
+      tea = create(:tea)
+
+      payload = {
+        title: 'The Good One',
+        price: 10.99,
+        status: 'active',
+        frequency: 'bi-monthly',
+        customer_id: customer.id,
+        tea_id: tea.id
+      }
+      headers = {"CONTENT_TYPE" => "application/json"}
+
+      post '/api/v0/subscriptions', headers: headers, params: JSON.generate(payload)
+
+      expect(response).to be_successful
+      expect(response.status).to eq(201)
+
+      sub = JSON.parse(response.body, symbolize_names: true)
+    end
+  end
 end
