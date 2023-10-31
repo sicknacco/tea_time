11719191529736a409cd2bc51604201e0659714b
Nick Sacco <nicksacco10@gmail.com>
2023-10-31 10:46:44 -0700

    Generated model for tea table creation and framework

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_31_173959) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_31_174548) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_31_173959) do
     t.string "last_name"
     t.string "email"
     t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "teas", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.string "temperature"
+    t.string "brew_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
