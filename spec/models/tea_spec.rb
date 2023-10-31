1f2d2d4300f6010a64fdabd26206b51c52f18310
Nick Sacco <nicksacco10@gmail.com>
2023-10-31 10:59:35 -0700

    test: configure shoulda-matchers and wrote relationship tests for teas

--- spec/models/tea_spec.rb
+++ spec/models/tea_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Tea, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it { should have_many(:subscriptions) }
+    it { should have_many(:customers).through(:subscriptions) }
+  end
 end
