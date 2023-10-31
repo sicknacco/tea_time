1dd359add5d89cb6bd67e31607721c9fd58c5019
Nick Sacco <nicksacco10@gmail.com>
2023-10-31 12:30:49 -0700

    test: complete happy path tests for subscription creation

--- spec/requests/subscription_request_spec.rb
+++ spec/requests/subscription_request_spec.rb
@@ -21,9 +21,33 @@ RSpec.describe "POST Subscription", type: :request do
       expect(response).to be_successful
       expect(response.status).to eq(201)
 
-      subscription = JSON.parse(response.body, symbolize_names: true)
+      sub = JSON.parse(response.body, symbolize_names: true)
 
-      expect(subscription).to be_a(Hash)
+      expect(sub).to be_a(Hash)
+      expect(sub).to have_key(:data)
+      expect(sub[:data]).to be_a(Hash)
+      expect(sub[:data]).to have_key(:id)
+      expect(sub[:data][:id]).to be_a(String)
+      expect(sub[:data]).to have_key(:type)
+      expect(sub[:data][:type]).to eq('subscription')
+      
+      expect(sub[:data]).to have_key(:attributes)
+      expect(sub[:data][:attributes]).to be_a(Hash)
+      expect(sub[:data][:attributes]).to have_key(:title)
+      expect(sub[:data][:attributes][:title]).to be_a(String)
+      expect(sub[:data][:attributes]).to have_key(:price)
+      expect(sub[:data][:attributes][:price]).to be_a(Float)
+      expect(sub[:data][:attributes]).to have_key(:status)
+      expect(sub[:data][:attributes][:status]).to be_a(String)
+      expect(sub[:data][:attributes]).to have_key(:frequency)
+      expect(sub[:data][:attributes][:frequency]).to be_a(String)
+
+      expect(sub[:data][:attributes]).to have_key(:customer_id)
+      expect(sub[:data][:attributes][:customer_id]).to be_a(Integer)
+      expect(sub[:data][:attributes][:customer_id]).to eq(customer.id)
+      expect(sub[:data][:attributes]).to have_key(:tea_id)
+      expect(sub[:data][:attributes][:tea_id]).to be_a(Integer)
+      expect(sub[:data][:attributes][:tea_id]).to eq(tea.id)
     end
   end
 end
