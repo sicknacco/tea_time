552d3ba1675cf141949d7f503154a8ceb36fd2e6
Nick Sacco <nicksacco10@gmail.com>
2023-10-31 12:24:39 -0700

    feat: filled in create action in subscription controller. following errors to serializer

--- app/controllers/api/v0/subscriptions_controller.rb
+++ app/controllers/api/v0/subscriptions_controller.rb
@@ -1,5 +1,16 @@
 class Api::V0::SubscriptionsController < ApplicationController
   def create
+    sub = Subscription.new(subscription_params)
+    if sub.save
+      render json: SubscriptionSerializer.new(sub), status: 201
+    else
+      "sad path"
+    end
+  end
 
+  private
+  
+  def subscription_params
+    params.permit(:title, :price, :status, :frequency, :customer_id, :tea_id)
   end
 end
