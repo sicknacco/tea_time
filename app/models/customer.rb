fea9eb2e2ecfa3d5669a7afea383557abc99275e
Nick Sacco <nicksacco10@gmail.com>
2023-10-31 13:06:52 -0700

    test/feat: customer validations created

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,9 @@
 class Customer < ApplicationRecord
   has_many :subscriptions
   has_many :teas, through: :subscriptions
+
+  validates_presence_of :first_name,
+                        :last_name,
+                        :email,
+                        :address
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -5,4 +5,11 @@ RSpec.describe Customer, type: :model do
     it { should have_many(:subscriptions) }
     it { should have_many(:teas).through(:subscriptions) }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:address) }
+  end
 end
