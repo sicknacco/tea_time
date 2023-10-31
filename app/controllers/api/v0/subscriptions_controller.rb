6714ec0018f20ffe7f52c280ca8f59bf06c92b3b
Nick Sacco <nicksacco10@gmail.com>
2023-10-31 12:19:54 -0700

    feat: Customize factories

--- app/controllers/api/v0/subscriptions_controller.rb
+++ app/controllers/api/v0/subscriptions_controller.rb
@@ -1,5 +1,5 @@
 class Api::V0::SubscriptionsController < ApplicationController
   def create
-    require 'pry'; binding.pry
+
   end
 end
--- spec/factories/teas.rb
+++ spec/factories/teas.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :tea do
-    title { "MyString" }
-    description { "MyString" }
-    temperature { "MyString" }
-    brew_time { "MyString" }
+    title { Faker::Tea.variety }
+    description { Faker::Tea.type }
+    temperature { Faker::Number.between(from: 150, to: 212) }
+    brew_time { Faker::Number.between(from: 1, to: 10) }
   end
 end
