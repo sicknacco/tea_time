e0b198cf300c9fa528c0661f11a98c36a503ea18
Nick Sacco <nicksacco10@gmail.com>
2023-10-31 13:09:21 -0700

    test/feat: validations for tea attributes with testing

--- spec/models/tea_spec.rb
+++ spec/models/tea_spec.rb
@@ -5,4 +5,11 @@ RSpec.describe Tea, type: :model do
     it { should have_many(:subscriptions) }
     it { should have_many(:customers).through(:subscriptions) }
   end
+
+  describe 'validations' do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:temperature) }
+    it { should validate_presence_of(:brew_time) }
+  end
 end
